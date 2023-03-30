bf28c4cf3c412130fda71cac695240ca7e028d67
Julien Madiot <Madi-Ji@users.noreply.github.com>
2023-03-30 17:30:32 +0000

    Improve JS errors handling via try/catch

--- nginx/njs/flagship.js
+++ nginx/njs/flagship.js
@@ -9,40 +9,41 @@ function convertCampaignVariationIntoCacheKey(
 }
 
 async function fetchExperienceCacheKey(r) {
-  ngx.log(ngx.WARN, JSON.stringify(r.headersIn));
-  let reply = await ngx.fetch(
-    `https://decision.flagship.io/v2/${r.variables.fs_env_id}/campaigns?mode=simple&exposeAllKeys=true`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        visitor_id: "@Madi-Ji",
-        context: {
-          github: "Madi-Ji/flagship-x-nginx-proxy-cache",
-        },
-        visitor_consent: false,
-        trigger_hit: false,
-      }),
-      headers: { "x-api-key": r.variables.fs_api_key },
-    }
-  );
+  //ngx.log(ngx.WARN, JSON.stringify(r.headersIn));
+  r.headersOut["X-fs-experiences"] = "optout";
+  try {
+    let reply = await ngx.fetch(
+      `https://decision.flagship.io/v2/${r.variables.fs_env_id}/campaigns?mode=simple&exposeAllKeys=true`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          visitor_id: "@Madi-Ji",
+          context: {
+            github: "Madi-Ji/flagship-x-nginx-proxy-cache",
+          },
+          visitor_consent: false,
+          trigger_hit: false,
+        }),
+        headers: { "x-api-key": r.variables.fs_api_key },
+      }
+    );
 
-  if (reply.status !== 200) {
-    // If something's wrong, optout
-    r.headersOut["X-fs-experiences"] = "optout";
-    r.return(reply.status);
-    return;
+    if (reply.status !== 200) throw reply.status;
+
+    let json = await reply.json();
+    let cacheKey = convertCampaignVariationIntoCacheKey(
+      json.campaignsVariation,
+      ":",
+      "|"
+    );
+    // Most of the Work done here.
+    r.headersOut["X-fs-experiences"] = cacheKey;
+    r.return(200);
+  } catch (e) {
+    r.error(e);
+    r.return(200);
   }
 
-  let json = await reply.json();
-
-  let cacheKey = convertCampaignVariationIntoCacheKey(
-    json.campaignsVariation,
-    ":",
-    "|"
-  );
-  // Most of the Work done here.
-  r.headersOut["X-fs-experiences"] = cacheKey;
-  r.return(200);
   return;
 }
 
